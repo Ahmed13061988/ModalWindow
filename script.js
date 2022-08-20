0407132daacd7ef0e3032524099208dd2416c66b
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-20 17:48:06 -0600

    refactoring the code

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', function () {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
-    btnCloseModal.addEventListener('click', function () {
-      modal.classList.add('hidden');
-      overlay.classList.add('hidden');
-    });
+  });
+
+  btnCloseModal.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
   });
 }
