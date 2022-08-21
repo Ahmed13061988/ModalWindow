e55b982919851ef11863bb48083e824f44c610b1
Ahmed Hussein <wolktheline@yahoo.com>
2022-08-20 18:01:09 -0600

    saving the function into variable to be more DRY

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  btnsOpenModal[i].addEventListener('click', openModal);
 
   const closeModal = function () {
     modal.classList.add('hidden');
